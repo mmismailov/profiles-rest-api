79dd0ba7c4de6efff37e5032aa0c49c05d13d5e9
Mehman <mmismailov@hmc.az>
2021-02-26 22:26:00 +0400

    Added Method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,12 +2,12 @@
 from rest_framework.views  import APIView
 # Create your views here.
 from rest_framework.response import Response
-
-
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
-
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView feature"""
@@ -17,6 +17,30 @@ class HelloApiView(APIView):
             'Gives you the most control over you application logic',
             'Is mapped manually to URLs',
         ]
-
-
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name= serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request,pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
